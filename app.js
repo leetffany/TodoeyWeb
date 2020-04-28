03846c6337b0b22d46c3ff063829593501edf75e
JamieKim1 <48039804+JamieKim1@users.noreply.github.com>
2020-04-28 13:41:07 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+
+const app = express();
+
+//still able to assign new although its const.
+const items = [];
+const workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+//app.use(bodyParser.json());
+
+app.get("/", function(req, res) {
+  //res.write("<h2>hi</h2>");
+  let day = date.getDate();
+
+  res.render("list", {
+    listTitle: day,
+    newListItem: items
+  });
+
+});
+
+app.post("/", function(req, res) {
+  const item = req.body.newItem;
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItem: workItems
+  });
+})
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+
+
+app.listen(3000, function() {
+  console.log("server started on port 3000");
+});
---
+++ date.js
@@ -0,0 +1,44 @@
+//jshint esversion:6
+//module.exports.getDate = function () {
+exports.getDate = function () {
+
+  const today = new Date();
+  //var currentDay = today.getDay();
+  //res.sendFile(__dirname+"/index.html");
+
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  };
+
+  return today.toLocaleDateString("en-US", options); //4/28/2020
+}
+
+
+
+
+
+
+module.exports.getDay =getDay;
+
+function getDay() {
+
+  const today = new Date();
+  //var currentDay = today.getDay();
+  //res.sendFile(__dirname+"/index.html");
+
+  const options = {
+    day: "numeric"
+  };
+
+  const day = today.toLocaleDateString("en-US", options); //4/28/2020
+  console.log(today.toLocaleDateString("en-US", options));
+  console.log(today.toDateString("ko-KR", options));
+  //console.log(today.toDateString("zh-CN",options));
+
+  return day;
+}
+
+//console.log(module.exports);
